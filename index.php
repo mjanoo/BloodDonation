290159a9a9e46e51223131f10043593854c04709
Janet <janet2wambua@gmail.com>
2021-04-13 23:36:41 +0300

    updated

--- index.php
+++ index.php
@@ -44,16 +44,10 @@
                                         <li class="nav-item">
                                             <a class="nav-link" href="#about">About Us</a>
                                         </li>
-                                       
                                         <li class="nav-item">
-                                            <a class="nav-link" href="#gallery">Gallery</a>
-                                        </li>
-                                         <li class="nav-item">
                                             <a class="nav-link" href="#process">Process</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#blog">Blog</a>
-                                        </li>
+                                        
                                         <li class="nav-item">
                                             <a class="btn btn-success   bounceInUp" href="register.php" role="button" > Become a Donor</a>
                                         </li>
@@ -166,7 +160,7 @@
     
       <!-- ################# Gallery Start Here #######################--->
      
-    <div id="gallery" class="gallery container-fluid">
+    <!-- <div id="gallery" class="gallery container-fluid">
         <div class="container">
             <div class="row session-title">
                 <h2>Checkout Our Gallery</h2>
@@ -203,14 +197,13 @@
                             </div>
                             <div class="gg-element">
                                 <img src="assets/images/gallery/g10.jpg">
-                            </div>
-                            
+                            </div>                   
                             
                           </div>
             </div>
         </div>
     </div>
-    
+     -->
     
     
      <!-- ################# Donation Process Start Here #######################--->
@@ -260,90 +253,7 @@
          </div>
      </section>
      
-     
-     
-     
-         <!--################### Our Blog Starts Here #######################--->
-        <div id="blog" class="blog-container contaienr-fluid">
-            <div class="container">
-                <div class="session-title row">
-                  <h2>Latest Blog</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit velit consectetur adipiscing elit.</p>
-                </div>
-                <div class="row news-row">
-                    <div class="col-md-6">
-                        <div class="news-card">
-                            <div class="image">
-                                <img src="assets/images/blog/blog_01.jpg" alt="">
-                            </div>
-                            <div class="detail">
-                                <h3>Latest News about Smarteye</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit consectetur adipiscing elit. </p>
-                                <p class="footp">
-                                    27 Comments <span>/</span>
-                                    Blog Design <span>/</span>
-                                    Read More
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="news-card">
-                            <div class="image">
-                                <img src="assets/images/blog/blog_02.jpg" alt="">
-                            </div>
-                            <div class="detail">
-                                <h3>Apple Launch its New Phone</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit consectetur adipiscing elit. </p>
-                                <p class="footp">
-                                    27 Comments <span>/</span>
-                                    Blog Design <span>/</span>
-                                    Read More
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="news-card">
-                            <div class="image">
-                                <img src="assets/images/blog/blog_03.jpg" alt="">
-                            </div>
-                            <div class="detail">
-                                <h3>About Windows 10 Update</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit consectetur adipiscing elit. </p>
-                                <p class="footp">
-                                    27 Comments <span>/</span>
-                                    Blog Design <span>/</span>
-                                    Read More
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="news-card">
-                            <div class="image">
-                                <img src="assets/images/blog/blog_04.jpg" alt="">
-                            </div>
-                            <div class="detail">
-                                <h3>Latest News about Smarteye</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit consectetur adipiscing elit. </p>
-                                <p class="footp">
-                                    27 Comments <span>/</span>
-                                    Blog Design <span>/</span>
-                                    Read More
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-     
-     
-
-   
-      <!--*************** Footer  Starts Here *************** -->
+     <!--*************** Footer  Starts Here *************** -->
     <footer id="contact" class="container-fluid">
         <div class="container">
             
